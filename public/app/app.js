8bbe4761e6c22e4ac28d8bc90f453954afacd8d6
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-08-06 16:30:06 +0300

    Add ui-router states to app

--- public/app/app.js
+++ public/app/app.js
@@ -14,6 +14,29 @@
                 templateUrl: '/app/templates/home.html',
                 controller: 'HomeController',
                 controllerAs: 'home'
+            })
+            .state('schools', {
+                url: '/schools',
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: 'app/templates/allSchools.html'
+            })
+            .state('classrooms', {
+                url: '/classrooms',
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: 'app/templates/allClassrooms.html'
+            })
+            .state('activities', {
+                url: '/activities',
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: 'app/templates/allActivities.html',
+                resolve: {
+                    activities: function (dataService) {
+                        return dataService.getAllActivities();
+                    }
+                },
             });
 
 
