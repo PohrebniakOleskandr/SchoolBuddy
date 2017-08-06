f0517856b0966c6a9f62822a2c25f04c58a708e3
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-08-06 11:31:34 +0300

    Add hash prefix mode

--- public/app/app.js
+++ public/app/app.js
@@ -2,9 +2,14 @@
 
     var app = angular.module('app', ['ngRoute']);
 
-    app.config(['$logProvider', '$routeProvider', function ($logProvider,$routeProvider) {
+    app.config([
+    '$logProvider', 
+    '$routeProvider', 
+    '$locationProvider',
+    function ($logProvider,$routeProvider,$locationProvider) {
 
         $logProvider.debugEnabled(true);
+        $locationProvider.hashPrefix('!');
 
         $routeProvider
             .when('/', {
@@ -31,7 +36,12 @@
             .when('/activities',{
                 controller: 'AllActivitiesController',
                 controllerAs: 'activities',
-                templateUrl: '/app/templates/allActivities.html' 
+                templateUrl: '/app/templates/allActivities.html',
+                resolve: {
+                    activities: function(dataService) {
+                        return dataService.getAllActivities();
+                    }
+                }
             })
             .when('/classrooms/:id',{
                 controller: 'ClassroomController',
